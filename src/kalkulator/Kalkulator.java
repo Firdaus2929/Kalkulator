c7ee0e89291905c0dc29d170088f23946dc916ef
Firdaus2929 <firdaushermawan29@gmail.com>
2021-11-16 00:35:38 +0700

    Project Kalkulator

---
+++ src/kalkulator/Kalkulator.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kalkulator;
+import kalkulator.Kalkulator1;
+/**
+ *
+ * @author ASUS
+ */
+public class Kalkulator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Kalkulator1 fKalkulator1 = new Kalkulator1 ();
+fKalkulator1.setVisible(true);
+    }
+    
+}
